7e08afa4d615582505487f1fdf7d7a5ea7705efd
Csanád Csuvarszki <csuvi98@hotmail.hu>
2026-02-25 20:22:50 +0100

    updated webconfig

--- backend/plant/src/main/java/com/plant/config/WebConfig.java
+++ backend/plant/src/main/java/com/plant/config/WebConfig.java
@@ -14,12 +14,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${app.localhost.url}")
     private String localhostUrl;
 
+    @Value("${app.frontend.www.url}")
+    private String frontendWwwUrl;
+
     // Allow localhost ports for frontend to send requets to the backend
     // Currently it's localhost, will be changed to something else once hosted
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl, localhostUrl)
+                .allowedOrigins(frontendUrl, localhostUrl,
+                        frontendWwwUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
